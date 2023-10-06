2a2641463cf8365109302ccb28764c6333208f03
Vlad Shkabara <schkabara14@gmail.com>
2023-10-06 11:10:15 +0300

    Added files

---
+++ src/main/java/com/example/SportApp/models/Post.java
@@ -0,0 +1,10 @@
+package com.example.SportApp.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Post {
+    private Long id;
+    private String name;
+    private
+}
---
+++ src/test/java/com/example/SportApp/SportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SportApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
